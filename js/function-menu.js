c690c21dcc369b18538344b5e9f8908f05ae7bf0
EriikGabriel <erikgabriel.lins@hotmail.com>
2020-07-29 23:05:43 -0300

    Commit - Finalizando o CRUD Cliente

--- js/function-menu.js
+++ js/function-menu.js
@@ -1,8 +1,13 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  $(".categoria").click(function (e) {
+    e.preventDefault();
+    $("#conteudo").empty();
+    $("#conteudo").load("src/categorias/visao/list-categoria.html");
+  });
 
-    $('.categoria').click(function(e) {
-        e.preventDefault()
-        $('#conteudo').empty()
-        $('#conteudo').load('src/categorias/visao/list-categoria.html')
-    })
-})
+  $(".cliente").click(function (e) {
+    e.preventDefault();
+    $("#conteudo").empty();
+    $("#conteudo").load("src/clientes/visao/list-cliente.html");
+  });
+});
---
+++ src/clientes/controle/create-cliente.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+  $(".btn-save").click(function (e) {
+    e.preventDefault();
+
+    let dados = $("#form-cliente").serialize();
+
+    $("input[type=checkbox]").each(function () {
+      if (!this.checked) {
+        dados += "&" + this.name + "=off";
+      }
+    });
+
+    $.ajax({
+      type: "POST",
+      dataType: "json",
+      assync: true,
+      data: dados,
+      url: "src/clientes/modelo/create-cliente.php",
+      success: function (dados) {
+        Swal.fire({
+          title: "appAulaDS",
+          text: dados.mensagem,
+          type: dados.tipo,
+          confirmButtonText: "OK",
+        }).then((result) => {
+          if (result.value) {
+            $("#conteudo").load("src/clientes/visao/list-cliente.html");
+          }
+        });
+
+        $("#modal-cliente").modal("hide");
+      },
+    });
+  });
+});
